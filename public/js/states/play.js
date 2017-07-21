97c7684895fff73d60ff2c0eb5623d6bc3f09db5
Matthew Hummer <biggreyson@gmail.com>
2017-07-21 15:50:13 -0600

    make bullet come out of randomly generated tank

--- public/js/states/play.js
+++ public/js/states/play.js
@@ -3,17 +3,38 @@ TanksGame.Play = function(game){
   this.score = 0;
   this.titleText = null;
   this.ready = false;
+  let randx = Math.floor(Math.random()*600);
+  let randy = Math.floor(Math.random()*600);
+  this.tank ={
+    position:{
+      x:randx,
+      y:randy
+    }
+  }
+
+  // this.tank.position.x = randx;
+  // this.tank.position.y = randy;
 }
 TanksGame.Play.prototype = {
   preload: function(){
-    this.load.image('bg', '/assets/background.png');
-    this.load.spritesheet('blueTank', '/assets/blue_tank/blue_tank.png', 32,32,3)
+    this.load.image('bg', '/assets/backgroundg.png');
+    this.load.image('bullet', '/assets/bullet.png');
+    this.load.spritesheet('blueTank', '/assets/blue_tank.png', 32,32,3);
   },
   create: function(){
     this.add.sprite(0,0,'bg');
-    let tank = this.add.sprite(300, 300, 'blueTank');
+    let tank = this.add.sprite(this.tank.position.x, this.tank.position.y, 'blueTank');
     tank.anchor.x = 0.5;
     tank.anchor.y = 0.5;
+    var fireButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    fireButton.onDown.add(this.fire, this)
   },
-  update: function(){}
+  update: function(){},
+  fire: function(thing){
+    console.log(thing);
+    console.log('fire');
+    let bullet = this.add.sprite(this.tank.position.x, this.tank.position.y-25, 'bullet');
+    bullet.anchor.x = 0.5;
+    bullet.anchor.y = 0.5;
+  }
 }
