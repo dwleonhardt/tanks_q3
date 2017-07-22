c936deec5f28411c5aa66d7ac1dcb03e1182a145
Arlo Shefte-Jacobs <arlo.sj3@gmail.com>
2017-07-22 14:27:42 -0600

    moving tank!

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,6 @@
 // console.log('game.js');
 window.onload = function(){
-  var game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameContainer');
+  var game = new Phaser.Game(1300, 700, Phaser.AUTO, 'gameContainer');
   console.log("onload");
   game.state.add('Boot', TanksGame.Boot);
   game.state.add('Play', TanksGame.Play);
