3880d72073a25894f2bc3f129a152997555b0959
dwleonhardt <dwleonhardt@gmail.com>
2017-07-22 14:35:08 -0600

    add direction

--- public/js/states/play.js
+++ public/js/states/play.js
@@ -1,4 +1,10 @@
 // console.log('play.js');
+var leftKey;
+var rightKey;
+var downKey;
+var upKey;
+var move = false;
+var pressed = false;
 TanksGame.Play = function(game){
   this.score = 0;
   this.titleText = null;
@@ -20,32 +26,44 @@ TanksGame.Play.prototype = {
     tank.animations.add('right',[9,10,11], 9, true);
     tank.animations.play('up', 9, false);
 
-    var leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
-    var downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
-    var rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
-    var upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
+    leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+    downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+    rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+    upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
 
     leftKey.onDown.add(function(move){
       tank.animations.add('left',[3,4,5], 9, true);
       tank.animations.play('left', 9, false);
     }, this)
 
+    TanksGame.leftKey.onDown.add(()=> pressed = true , this);
+    TanksGame.leftKey.onUp.add(()=> pressed = false , this);
+
     downKey.onDown.add(function(move){
       tank.animations.add('down',[6,7,8], 9, true);
-      tank.animations.play('down', 9, false);
+      tank.animations.play('down', 9, move);
     }, this)
 
     rightKey.onDown.add(function(move){
       tank.animations.add('right',[9,10,11], 9, true);
-      tank.animations.play('right', 9, false);
+      tank.animations.play('right', 9, move);
     }, this)
 
     upKey.onDown.add(function(move){
       tank.animations.add('up',[0,1,2], 9, true);
-      tank.animations.play('up', 9, false);
+      tank.animations.play('up', 9, move);
     }, this)
 
 
   },
-  update: function(){}
+  update: function(){
+    if(pressed = true){
+      console.log('on');
+      // move = true;
+    }
+    else{
+      console.log('off');
+    }
+  }
+
 }
