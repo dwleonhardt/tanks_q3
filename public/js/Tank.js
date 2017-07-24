abcb4c3e67f1f5aa1779f4836636fd152a70d241
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-07-24 14:21:30 -0600

    add rotations to tank constructor

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -4,17 +4,12 @@ TanksGame.Tank = function (game, tank) {
 }
 
 TanksGame.Tank.prototype = {
-  move: function (tank) {
-    var leftKey = tank.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-    leftKey.onDown.add(function(move){
-      console.log('left');
-      tank.animations.add('left',[3,4,5], 9);
-      tank.animations.play('left', 9, true);
-    }, this)
-    leftKey.onUp.add(function(){
-      tank.animations.stop(null, true);
-    }, this)
+  rotateLeft: function(tank){
+    
+    tank.angle -= 4;
 
-
+  },
+  rotateRight: function(tank){
+    tank.angle += 4;
   }
 }
