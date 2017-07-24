b52e2cdf93e6bdd2bc29e8fa834ca250b4bf8bdc
Adam Smith <asmith13194@gmail.com>
2017-07-24 15:26:53 -0600

    added velocity

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -4,12 +4,30 @@ TanksGame.Tank = function (game, tank) {
 }
 
 TanksGame.Tank.prototype = {
-  rotateLeft: function(tank){
-    
-    tank.angle -= 4;
-
-  },
-  rotateRight: function(tank){
-    tank.angle += 4;
-  }
+  // rotateLeft: function(tank){
+  //
+  //   tank.angle -= 4;
+  //
+  // },
+  // rotateRight: function(tank){
+  //   tank.angle += 4;
+  // }
+  // tank.body.velocity.x = 0;
+  // tank.body.velocity.y = 0;
+  // tank.body.angularVelocity = 0;
+  // tank.angle;
+  //
+  // if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+  // {
+  //     tank.body.angularVelocity = -200;
+  // }
+  // else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+  // {
+  //     tank.body.angularVelocity = 200;
+  // }
+  //
+  // if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+  // {
+  //     game.physics.arcade.velocityFromAngle(tank.angle-90, 300, tank.body.velocity);
+  // }
 }
