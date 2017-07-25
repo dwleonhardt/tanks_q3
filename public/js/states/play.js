2fbc28748597318a6b78cffd497ae13004239622
Adam Smith <asmith13194@gmail.com>
2017-07-24 18:33:29 -0600

    create enemy constructor

--- public/js/states/play.js
+++ public/js/states/play.js
@@ -22,8 +22,10 @@ TanksGame.Play.prototype = {
   ready: false,
   preload: function(){
     this.load.spritesheet('blueTank', '/assets/blue_tank.png', 50,50,12);
+    this.load.spritesheet('redTank', '/assets/red_tank.png', 50,50,12);
     this.load.spritesheet('bullet', '/assets/bullet.png', 50, 50, 1);
     this.load.spritesheet('blueTurret', '/assets/blue_tank_top.png', 50, 50, 1);
+    this.load.spritesheet('redTurret', '/assets/red_tank_top.png', 50, 50, 1);
   },
   create: function(){
     allTanks = {};
@@ -31,6 +33,7 @@ TanksGame.Play.prototype = {
     Client.addPlayer();
   },
   addMe: (x,y,id)=>{
+    enemy = new TanksGame.EnemyTank(x,y,id);
     TanksGame.Play.prototype.allTanks[id]=new TanksGame.Tank(x,y,id);
     TanksGame.Play.prototype.ready = true;
   },
