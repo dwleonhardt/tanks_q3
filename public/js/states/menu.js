27ddcb47d210f847b9e1bede0d59e7d5d6c608d6
Matthew Hummer <biggreyson@gmail.com>
2017-07-25 10:52:16 -0600

    add other tank colors to be dynamically added

---
+++ public/js/states/menu.js
@@ -0,0 +1,9 @@
+console.log('menu');
+TanksGame.Menu = function(game){
+
+}
+TanksGame.Menu.prototype = {
+  preload: function(){},
+  create: function(){},
+  update: function(){}
+}
--- public/js/states/play.js
+++ public/js/states/play.js
@@ -6,16 +6,19 @@ TanksGame.Play = function(game){
   this.direction = 'up';
 }
 
-console.log('here');
 TanksGame.Play.prototype = {
   allTanks: {},
   ready: false,
   preload: function(){
     this.load.spritesheet('blueTank', '/assets/blue_tank.png', 50,50,12);
     this.load.spritesheet('redTank', '/assets/red_tank.png', 50,50,12);
+    this.load.spritesheet('silverTank', '/assets/silver_tank.png', 50,50,12);
+    this.load.spritesheet('greenTank', '/assets/green_tank.png', 50,50,12);
     this.load.spritesheet('bullet', '/assets/bullet.png', 50, 50, 1);
     this.load.spritesheet('blueTurret', '/assets/blue_tank_top.png', 50, 50, 1);
     this.load.spritesheet('redTurret', '/assets/red_tank_top.png', 50, 50, 1);
+    this.load.spritesheet('silverTurret', '/assets/silver_tank_top.png', 50, 50, 1);
+    this.load.spritesheet('greenTurret', '/assets/green_tank_top.png', 50, 50, 1);
   },
   create: function(){
     allTanks = {};
@@ -26,12 +29,12 @@ TanksGame.Play.prototype = {
     delete TanksGame.Play.prototype.allTanks[id];
     game.world.children = game.world.children.filter((sprite)=>{return sprite.id != id});
   },
-  addFoe: function(x,y,id){
-    enemy = new TanksGame.EnemyTank(x,y,id);
+  addFoe: function(x,y,id,color){
+    enemy = new TanksGame.EnemyTank(x,y,id,'green');
     TanksGame.Play.prototype.allTanks[id] = enemy;
   },
-  addMe: (x,y,id)=>{
-    let me = new TanksGame.Tank(x,y,id);
+  addMe: (x,y,id,color)=>{
+    let me = new TanksGame.Tank(x,y,id, 'green');
     TanksGame.Play.prototype.allTanks[id]= me;
     TanksGame.Play.prototype.ready = true;
   },
