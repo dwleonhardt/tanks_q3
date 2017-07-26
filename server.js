4c5e4277791fef6e834d23922373eea631ecb196
dwleonhardt <dwleonhardt@gmail.com>
2017-07-26 14:24:45 -0600

    works dope

--- server.js
+++ server.js
@@ -102,8 +102,8 @@ io.on('connection', function(socket){
 
 
 if (process.env.NODE_ENV !== 'production') {
-  server.listen(port,'10.9.22.254', ()=>{
-    console.log('listening on ', '10.9.22.254:'+port);
+  server.listen(port,'10.9.23.162', ()=>{
+    console.log('listening on ', '10.9.23.162:'+port);
   });
 }
 else{
