3fce63ac3d08c97267d6d71f25abcfe73bb8509e
Matthew Hummer <biggreyson@gmail.com>
2017-07-27 21:26:40 -0600

    re add splash

--- public/js/states/Boot.js
+++ public/js/states/Boot.js
@@ -1,5 +1,4 @@
 var TanksGame = {};
-console.log('cats');
 
 TanksGame.Boot = function(game){
 
@@ -16,6 +15,6 @@ TanksGame.Boot.prototype = {
     this.pageAlignVertically = true;
     this.stage.forcePortrait = true;
     this.stage.backgroundColor = '#553EB4';
-    this.state.start('Menu');
+    this.state.start('Splash');
   }
 };
